dbde9816bf43a3bae253a83efa8f95c4a5b15662
liljoker06 <lahrimialadin@gmail.com>
2025-03-18 20:09:34 +0100

    Mise à jour du logo et ajustement de la taille dans le composant Logo

--- myTaskApp/components/Logo.tsx
+++ myTaskApp/components/Logo.tsx
@@ -1,13 +1,12 @@
-// components/Logo.tsx
 import React from 'react';
 import { View, Image } from 'react-native';
 
 export default function Logo() {
   return (
-    <View style={{ marginBottom: 32, alignItems: 'center' }}>
+    <View style={{ marginBottom: 0, alignItems: 'center' }}> 
       <Image 
         source={require('../assets/logo.png')} 
-        style={{ width: 150, height: 150, resizeMode: 'contain' }} 
+        style={{ width: 250, height: 250, resizeMode: 'contain' }} 
       />
     </View>
   );
